837b92af4381118c7982d52b9db7688018add9c4
yuki shiozaki <manten11@gmail.com>
2019-05-25 05:28:10 +0000

    first commit

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>課題</title>
+    </head>
+    <body>
+        <h1>課題４応用作成その１, Add action</h1>
+    </body>
+    <p>今回の課題です????</p>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>課題</title>
+    </head>
+    <body>
+        <h1>課題４応用作成その2, Edit action</h1>
+    </body>
+</html>

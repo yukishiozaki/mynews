4f65d61ba2933ab6b7569dc4655e9b8941d2d3b6
yuki shiozaki <manten11@gmail.com>
2019-05-30 21:51:37 +0000

    PHP Laravel11_first commit

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
 
-        <title>課題</title>
-    </head>
-    <body>
-        <h1>課題４応用作成その１, Add action</h1>
-    </body>
-</html>
+{{-- admin/profile.create2.blade.phpの@yield('title'に'プロフィール'を埋め込む --}}
+@section('title', 'プロフィール')
+
+{{-- admin/profile.create2.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h1>Myプロフィール</h1></h1>
+            </div>
+        </div>
+    </div>
+@endsection

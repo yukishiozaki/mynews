2061b7fe01e9369d2949317909fa6431ddd7525d
yuki shiozaki <manten11@gmail.com>
2019-06-09 05:36:10 +0000

    commit

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,15 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

484f5552ede01578080a5f2655d8f5cdb0742bfb
yuki shiozaki <manten11@gmail.com>
2019-06-13 12:00:09 +0000

    PHP/Laravel13

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,9 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() 
 {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
 Auth::routes();

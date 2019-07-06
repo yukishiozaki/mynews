dbbdb8404f2dcb89b35c54bdbb94069d2f64974f
yuki shiozaki <manten11@gmail.com>
2019-07-06 09:27:58 +0000

    commit

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'NewsController@index');

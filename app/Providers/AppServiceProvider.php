e39426e214a75be3d486df679b616adf7492bbd8
yuki shiozaki <manten11@gmail.com>
2019-07-08 13:37:02 +0000

    second commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+     // 以下を追記
         if (\App::environment('production')) {
-            \URL::forceScheme('https');
+           \URL::forceScheme('https');
         }
     }
 }

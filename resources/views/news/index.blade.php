0ade27a359a06f6ad77b252a530454e063d7c9e9
yuki shiozaki <manten11@gmail.com>
2019-07-13 07:17:04 +0000

    for heroku

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -11,7 +11,7 @@
                             <div class="caption mx-auto">
                                 <div class="image">
                                     @if ($headline->image_path)
-                                        <img src="{{ $headline->image_path) }}">
+                                        <img src="{{ $headline->image_path }}">
                                     @endif
                                 </div>
                                 <div class="title p-2">
